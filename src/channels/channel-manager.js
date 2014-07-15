57adaa0bfc159b611d6d7f60ab823ded817caa58
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-15 11:48:25 -0700

    refactor create

--- src/channels/channel-manager.js
+++ src/channels/channel-manager.js
@@ -15,30 +15,27 @@ module.exports = function(config) {
     //TODO: store runid in token etc
     var $creationPromise = rs.create({model: 'pdasim.vmf'});
 
+    var createAndThen = function(value) {
+        return _.wrap(value, function(func) {
+            var passedInParams = _.toArray(arguments).slice(1);
+            return $creationPromise.done(function (){
+                return func.apply(rs, passedInParams);
+            });
+        });
+    };
+
     //Make sure nothing happens before the run is created
     _.each(rs, function(value, name) {
         if ($.isFunction(value) && name !== 'variables') {
-            rs[name] = _.wrap(value, function(func) {
-                var passedInParams = _.toArray(arguments).slice(1);
-                return $creationPromise.done(function (){
-                    return func.apply(rs, passedInParams);
-                });
-            });
+            rs[name] = createAndThen(value);
         }
     });
-
     var vs = rs.variables();
     _.each(vs, function(value, name) {
         if ($.isFunction(value)) {
-            vs[name] = _.wrap(value, function(func) {
-                var passedInParams = _.toArray(arguments).slice(1);
-                return $creationPromise.done(function (){
-                    return func.apply(vs, passedInParams);
-                });
-            });
+            vs[name] = createAndThen(value);
         }
     });
-
 
     this.run = rs;
     this.variables = new VarsChannel({variables: vs});
