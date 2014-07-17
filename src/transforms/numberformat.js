a9aa7d0b938a6add7b21cd6f305b7fedcddc829f
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-17 12:00:25 -0700

    number formatter transform

---
+++ src/transforms/numberformat.js
@@ -0,0 +1,237 @@
+'use strict';
+module.exports = {
+    test: '',
+
+    format: (function(value) {
+        var scales = ['', 'K', 'M', 'B', 'T'];
+
+        function getDigits(value, digits) {
+            value = value === 0 ? 0 : roundTo(value, Math.max(0, digits - Math.ceil(Math.log(value) / Math.LN10)));
+
+            var TXT = '';
+            var numberTXT = value.toString();
+            var decimalSet = false;
+
+            for (var iTXT = 0; iTXT < numberTXT.length; iTXT++) {
+                TXT += numberTXT.charAt(iTXT);
+                if (numberTXT.charAt(iTXT) === '.') {
+                    decimalSet = true;
+                } else {
+                    digits--;
+                }
+
+                if (digits <= 0) {
+                    return TXT;
+                }
+            }
+
+            if (!decimalSet) {
+                TXT += '.';
+            }
+            while (digits > 0) {
+                TXT += '0';
+                digits--;
+            }
+            return TXT;
+        }
+
+        function addDecimals(value, decimals, minDecimals, hasCommas) {
+            hasCommas = hasCommas || true;
+            var numberTXT = value.toString();
+            var hasDecimals = (numberTXT.split('.').length > 1);
+            var iDec = 0;
+
+            if (hasCommas) {
+                for (var iChar = numberTXT.length - 1; iChar > 0; iChar--) {
+                    if (hasDecimals) {
+                        hasDecimals = !(numberTXT.charAt(iChar) === '.');
+                    } else {
+                        iDec = (iDec + 1) % 3;
+                        if (iDec === 0) {
+                            numberTXT = numberTXT.substr(0, iChar) + ',' + numberTXT.substr(iChar);
+                        }
+                    }
+                }
+
+            }
+            if (decimals > 0) {
+                var toADD;
+                if (numberTXT.split('.').length <= 1) {
+                    toADD = minDecimals;
+                    if (toADD > 0) {
+                        numberTXT += '.';
+                    }
+                } else {
+                    toADD = minDecimals - numberTXT.split('.')[1].length;
+                }
+
+                while (toADD > 0) {
+                    numberTXT += '0';
+                    toADD--;
+                }
+            }
+            return numberTXT;
+        }
+
+        function roundTo(value, digits) {
+            return Math.round(value * Math.pow(10, digits)) / Math.pow(10, digits);
+        }
+
+        function getSuffix(formatTXT) {
+            formatTXT = formatTXT.replace('.', '');
+            var fixesTXT = formatTXT.split(new RegExp('[0|,|#]+', 'g'));
+            return (fixesTXT.length > 1) ? fixesTXT[1].toString() : '';
+        }
+
+        function isCurrency(string) {
+            var s = $.trim(string);
+
+            if (s === '$' ||
+                s === 'â‚¬' ||
+                s === 'Â¥' ||
+                s === 'Â£' ||
+                s === 'â‚¡' ||
+                s === 'â‚±' ||
+                s === 'KÄ?' ||
+                s === 'kr' ||
+                s === 'Â¢' ||
+                s === 'â‚ª' ||
+                s === 'Æ’' ||
+                s === 'â‚©' ||
+                s === 'â‚«') {
+
+                return true;
+            }
+
+            return false;
+        }
+
+        function format(number, formatTXT) {
+            var originalNumber = number,
+                originalFormat = formatTXT;
+            if (!formatTXT || formatTXT.toLowerCase() === 'default') {
+                return number.toString();
+            }
+
+            if (isNaN(number)) {
+                return '?';
+            }
+
+            //var formatTXT;
+            formatTXT = formatTXT.replace('&euro;', 'â‚¬');
+
+            // Divide +/- Number Format
+            var formats = formatTXT.split(';');
+            if (formats.length > 1) {
+                return format(Math.abs(number), formats[((number >= 0) ? 0 : 1)]);
+            }
+
+            // Save Sign
+            var sign = (number >= 0) ? '' : '-';
+            number = Math.abs(number);
+
+
+            var leftOfDecimal = formatTXT;
+            var d = leftOfDecimal.indexOf('.');
+            if (d > -1) {
+                leftOfDecimal = leftOfDecimal.substring(0, d);
+            }
+
+            var normalized = leftOfDecimal.toLowerCase();
+            var index = normalized.lastIndexOf('s');
+            var isShortFormat = index > -1;
+
+            if (isShortFormat) {
+                var nextChar = leftOfDecimal.charAt(index + 1);
+                if (nextChar === ' ') {
+                    isShortFormat = false;
+                }
+            }
+
+            var leadingText = isShortFormat ? formatTXT.substring(0, index) : '';
+            var rightOfPrefix = isShortFormat ? formatTXT.substr(index + 1) : formatTXT.substr(index);
+
+            //first check to make sure 's' is actually short format and not part of some leading text
+            if (isShortFormat) {
+                var shortFormatTest = /[0-9#*]/;
+                var shortFormatTestResult = rightOfPrefix.match(shortFormatTest);
+                if (!shortFormatTestResult || shortFormatTestResult.length === 0) {
+                    //no short format characters so this must be leading text ie. 'weeks '
+                    isShortFormat = false;
+                    leadingText = '';
+                }
+            }
+
+            //if (formatTXT.charAt(0) == 's')
+            if (isShortFormat) {
+                var valScale = number === 0 ? 0 : Math.floor(Math.log(Math.abs(number)) / (3 * Math.LN10));
+                valScale = ((number / Math.pow(10, 3 * valScale)) < 1000) ? valScale : (valScale + 1);
+                valScale = Math.max(valScale, 0);
+                valScale = Math.min(valScale, 4);
+                number = number / Math.pow(10, 3 * valScale);
+                //if (!isNaN(Number(formatTXT.substr(1) ) ) )
+
+                if (!isNaN(Number(rightOfPrefix)) && rightOfPrefix.indexOf('.') === -1) {
+                    var limitDigits = Number(rightOfPrefix);
+                    if (number < Math.pow(10, limitDigits)) {
+                        if (isCurrency(leadingText)) {
+                            return sign + leadingText + getDigits(number, Number(rightOfPrefix)) + scales[valScale];
+                        }
+                        else {
+                            return leadingText + sign + getDigits(number, Number(rightOfPrefix)) + scales[valScale];
+                        }
+                    } else {
+                        if (isCurrency(leadingText)) {
+                            return sign + leadingText + Math.round(number) + scales[valScale];
+                        }
+                        else {
+                            return leadingText + sign + Math.round(number) + scales[valScale];
+                        }
+                    }
+                } else {
+                    //formatTXT = formatTXT.substr(1);
+                    formatTXT = formatTXT.substr(index + 1);
+                    var SUFFIX = getSuffix(formatTXT);
+                    formatTXT = formatTXT.substr(0, formatTXT.length - SUFFIX.length);
+
+                    var valWithoutLeading = format(((sign == '') ? 1 : -1) * number, formatTXT) + scales[valScale] + SUFFIX;
+                    if (isCurrency(leadingText) && sign !== '') {
+                        valWithoutLeading = valWithoutLeading.substr(sign.length);
+                        return sign + leadingText + valWithoutLeading;
+                    }
+
+                    return leadingText + valWithoutLeading;
+                }
+            }
+
+            var subFormats = formatTXT.split('.');
+            var decimals;
+            var minDecimals;
+            if (subFormats.length > 1) {
+                decimals = subFormats[1].length - subFormats[1].replace(new RegExp('[0|#]+', 'g'), '').length;
+                minDecimals = subFormats[1].length - subFormats[1].replace(new RegExp('0+', 'g'), '').length;
+                formatTXT = subFormats[0] + subFormats[1].replace(new RegExp('[0|#]+', 'g'), '');
+            } else {
+                decimals = 0;
+            }
+
+            var fixesTXT = formatTXT.split(new RegExp('[0|,|#]+', 'g'));
+            var preffix = fixesTXT[0].toString();
+            var suffix = (fixesTXT.length > 1) ? fixesTXT[1].toString() : '';
+
+            number = number * ((formatTXT.split('%').length > 1) ? 100 : 1);
+            //            if(formatTXT.indexOf('%') !== -1) number = number * 100;
+            number = roundTo(number, decimals);
+
+            sign = (number === 0) ? '' : sign;
+
+            var hasCommas = (formatTXT.substr(formatTXT.length - 4 - suffix.length, 1) === ',');
+            var formatted = sign + preffix + addDecimals(number, decimals, minDecimals, hasCommas) + suffix;
+
+            //  console.log(originalNumber, originalFormat, formatted)
+            return formatted;
+        }
+
+        return format;
+    }())
+};
