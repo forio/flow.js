d6e1a8207779770dae6a083590f2e1b0b0dc9303
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-22 14:32:09 -0700

    Don't pass in empty params for args

--- src/dom/attributes/events/default-event-attr.js
+++ src/dom/attributes/events/default-event-attr.js
@@ -13,8 +13,9 @@ module.exports = {
         var me = this;
         this.on(attr, function() {
             var fnName = value.split('(')[0];
-            var params = value.substring(value.indexOf('(') + 1, value.indexOf(')')).split(',');
-            me.trigger('f.ui.operate', {fn: fnName, args: params});
+            var params = value.substring(value.indexOf('(') + 1, value.indexOf(')'));
+            var args = ($.trim(params) !== '') ? params.split(',') : [];
+            me.trigger('f.ui.operate', {fn: fnName, args: args});
         });
         return false; //Don't bother binding on this attr. NOTE: Do readonly, true instead?;
     }
--- src/dom/attributes/events/init-event-attr.js
+++ src/dom/attributes/events/init-event-attr.js
@@ -14,8 +14,9 @@ module.exports = {
         $(function () {
             var fnName = value.split('(')[0];
             var params = value.substring(value.indexOf('(') + 1, value.indexOf(')')).split(',');
+            var args = ($.trim(params) !== '') ? params.split(',') : [];
 
-            me.trigger('f.ui.operate', {fn: fnName, args: params});
+            me.trigger('f.ui.operate', {fn: fnName, args: args});
         });
         return false; //Don't bother binding on this attr. NOTE: Do readonly, true instead?;
     }
