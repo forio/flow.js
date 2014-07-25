d4849dd8e0e346da880c94c01f83964e4a36db1f
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-24 17:55:41 -0700

    refactor input node

--- tests/specs/dom/test-default-input-node.js
+++ tests/specs/dom/test-default-input-node.js
@@ -1,19 +1,57 @@
 (function() {
     'use strict';
 
-    var nodeClass = require('../../../src/dom/nodes/default-input-node');
-    var utils = require('../../testing-utils');
+    var NodeClass = require('../../../src/dom/nodes/default-input-node');
+    var make = require('../../testing-utils').create;
 
     describe('Input node', function () {
-        var textNode;
+        var textNode, makeView;
         before(function (){
-            textNode = utils.create('<input type="text" data-f-bind="stuff"/>');
+            textNode = make('<input type="text" data-f-bind="stuff"/>');
+
+            makeView = function (str) {
+                var node = make(str);
+                var nodeView = new NodeClass({el: node});
+                return nodeView;
+            };
         });
 
-        describe('claim', function () {
+        after(function (){
+            textNode = makeView = null;
+        });
+
+        describe('selector', function () {
             it('should claim input nodes', function () {
-                var claimed = $(textNode).is(nodeClass.selector);
+                var claimed = $(textNode).is(NodeClass.selector);
                 claimed.should.be.true;
+            });
+            it('should claim select nodes', function () {
+                var node = make('<select> <option> 1 </option> </select>');
+                var claimed = $(node).is(NodeClass.selector);
+                claimed.should.be.true;
+            });
+            it('should claim checkboxes', function () {
+                var node = make('<input type="checkbox" data-f-bind="stuff"/>');
+                var claimed = $(node).is(NodeClass.selector);
+                claimed.should.be.true;
+            });
+            it('should claim radio buttons', function () {
+                var node = make('<input type="radio" data-f-bind="stuff"/>');
+                var claimed = $(node).is(NodeClass.selector);
+                claimed.should.be.true;
+            });
+            it('should not claim divs', function () {
+                var node = make('<div> stuff </div>');
+                var claimed = $(node).is(NodeClass.selector);
+                claimed.should.be.false;
+            });
+        });
+
+        describe('getUIValue', function () {
+            it('should get value of textboxes as strings', function () {
+                var nv = makeView('<input type="text" value="5" data-f-bind="stuff"/>');
+                var val = nv.getUIValue();
+                val.should.equal('5');
             });
         });
     });
