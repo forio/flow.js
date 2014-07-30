8b1e4fea2856f9ffa4b1f99c6f0211db563e2b7c
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-30 13:42:37 -0700

    refactored textbox test

--- tests/specs/dom/element-tests/test-text.js
+++ tests/specs/dom/element-tests/test-text.js
@@ -1,36 +1,26 @@
 module.exports = (function() {
     'use strict';
     var utils = require('../../../testing-utils');
-    var make = utils.create;
-    var dummyChannelManager = utils.createDummyChannel();
     var domManager = require('../../../../src/dom/dom-manager');
 
     describe(':text', function () {
         describe('input handlers', function () {
             it('should trigger the right event on ui change', function () {
-                var $textNode = $(make('<input type="text" data-f-bind="stuff"/>'));
-                domManager.initialize({
-                    root: $textNode,
-                    channel: dummyChannelManager
-                });
+                var $node = utils.initWithNode('<input type="text" data-f-bind="stuff"/>', domManager);
+                var spy = utils.spyOnNode($node);
 
-                var spy = sinon.spy();
-                $textNode.on('update.f.ui', spy);
-                $textNode.trigger('change');
+                $node.trigger('change');
 
                 spy.should.have.been.called.once;
             });
 
             it('should pass the right params to the event', function () {
-                var $textNode = $(make('<input type="text" data-f-bind="stuff"/>'));
-                domManager.initialize({
-                    root: $textNode,
-                    channel: dummyChannelManager
-                });
-                var spy = sinon.spy();
-                $textNode.on('update.f.ui', spy);
-                $textNode.val(5);
-                $textNode.trigger('change');
+                var $node = utils.initWithNode('<input type="text" data-f-bind="stuff" value="3"/>', domManager);
+                var spy = utils.spyOnNode($node);
+
+                $node.val(5);
+                $node.trigger('change');
+
                 spy.getCall(0).args[1].should.eql({stuff: '5'});
             });
         });
