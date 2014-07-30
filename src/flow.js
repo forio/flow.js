7e7bf4d0871024f9fc8a5bf85ed80972591c06a8
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-30 16:08:46 -0700

    Seperated flow into browserify and normal versions

---
+++ src/flow.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var domManager = require('./dom/dom-manager');
+var Channel = require('./channels/channel-manager');
+
+module.exports = {
+    dom: domManager,
+
+    initialize: function(config) {
+        var model = $('body').data('f-model');
+
+        var defaults = {
+            channel: {
+                account: '',
+                project: '',
+                model: model
+            },
+            dom: {
+
+            }
+        };
+        var options = $.extend(true, {}, defaults, config);
+        this.channel = new Channel(options.channel);
+
+        domManager.initialize($.extend(true, {
+            channel: this.channel
+        }));
+    }
+};
