76800fe933dc16f26f14df95b46cea92c1418f0b
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-29 17:46:26 -0700

    cleaned up checkbox test

--- tests/specs/dom/element-tests/test-checkbox.js
+++ tests/specs/dom/element-tests/test-checkbox.js
@@ -1,32 +1,33 @@
 module.exports = (function() {
     'use strict';
     var utils = require('../../../testing-utils');
-    var make = utils.create;
-    var dummyChannelManager = utils.createDummyChannel();
     var domManager = require('../../../../src/dom/dom-manager');
 
     describe(':checkbox', function () {
-        var initWithNode;
+        var initWithNode, createSpy;
         before(function () {
             initWithNode = function (str) {
-                var $node = $(make(str));
+                var $node = $(utils.create(str));
                 domManager.initialize({
                     root: $node,
-                    channel: dummyChannelManager
+                    channel: utils.createDummyChannel()
                 });
                 return $node;
+            };
+            createSpy = function ($node) {
+                var spy = sinon.spy();
+                $node.on('update.f.ui', function(){
+                    //sinon doesn't like passing the spy directly with 'this' as context.
+                    spy.apply(null, arguments);
+                });
+                return spy;
             };
         });
 
         describe('input handlers', function () {
             it('should trigger the right event on ui change', function () {
                 var $node = initWithNode('<input type="checkbox" data-f-bind="stuff"/>');
-
-                var spy = sinon.spy();
-                $node.on('update.f.ui', function(){
-                    //sinon doesn't like passing the spy directly with 'this' as context.
-                    spy.apply(null, arguments);
-                });
+                var spy = createSpy($node);
                 $node.trigger('change');
                 spy.should.have.been.called.once;
             });
@@ -34,11 +35,8 @@ module.exports = (function() {
             describe('On Check', function () {
                 it('should pass the right value on check - no default', function () {
                     var $node = initWithNode('<input type="checkbox" data-f-bind="stuff"/>');
+                    var spy = createSpy($node);
 
-                    var spy = sinon.spy();
-                    $node.on('update.f.ui', function(){
-                        spy.apply(null, arguments);
-                    });
                     $node.prop('checked', true);
                     $node.trigger('change');
 
@@ -47,11 +45,8 @@ module.exports = (function() {
 
                 it('should pass the right value on check - default on', function () {
                     var $node = initWithNode('<input type="checkbox" data-f-bind="stuff" value="4"/>');
+                    var spy = createSpy($node);
 
-                    var spy = sinon.spy();
-                    $node.on('update.f.ui', function(){
-                        spy.apply(null, arguments);
-                    });
                     $node.prop('checked', true);
                     $node.trigger('change');
 
@@ -61,11 +56,8 @@ module.exports = (function() {
             describe('On UnCheck', function () {
                 it('should pass the right value on check - no default', function () {
                     var $node = initWithNode('<input type="checkbox" data-f-bind="stuff" checked/>');
+                    var spy = createSpy($node);
 
-                    var spy = sinon.spy();
-                    $node.on('update.f.ui', function(){
-                        spy.apply(null, arguments);
-                    });
                     $node.prop('checked', false);
                     $node.trigger('change');
 
@@ -74,11 +66,8 @@ module.exports = (function() {
 
                 it('should pass the right value on check - default off', function () {
                     var $node = initWithNode('<input type="checkbox" data-f-bind="stuff" data-f-off="5" checked/>');
+                    var spy = createSpy($node);
 
-                    var spy = sinon.spy();
-                    $node.on('update.f.ui', function(){
-                        spy.apply(null, arguments);
-                    });
                     $node.prop('checked', false);
                     $node.trigger('change');
 
