85f1a7bd356f83c9b552f413be06ff387202b47d
Naren Ranjit <narendran.ranjit@gmail.com>
2014-07-30 14:25:11 -0700

    Radio buttons test

---
+++ tests/specs/dom/element-tests/test-radio-button.js
@@ -0,0 +1,59 @@
+module.exports = (function() {
+    'use strict';
+    var utils = require('../../../testing-utils');
+    var domManager = require('../../../../src/dom/dom-manager');
+
+    describe(':radio', function () {
+        describe('input handlers', function () {
+            it('should trigger the right event on ui change', function () {
+                var nodes = [
+                    '<input type="radio" name="a" id="x" data-f-bind="stuff" value="1" checked/>',
+                    '<input type="radio" name="a" id="y" data-f-bind="stuff" value"2"/>'
+                ].join('');
+                var $node1 = utils.initWithNode(nodes, domManager).filter('#x');
+                var $node2 = utils.initWithNode(nodes, domManager).filter('#y');
+
+                var spy = utils.spyOnNode($node1);
+                utils.spyOnNode($node2, spy);
+
+                $node1.trigger('change');
+                spy.should.have.been.called.once;
+            });
+
+            describe('On Check', function () {
+                it('should pass the right value on check', function () {
+                    var nodes = [
+                        '<input type="radio" name="a" id="x" data-f-bind="stuff" value="8"/>',
+                        '<input type="radio" name="a" id="y" data-f-bind="stuff" value"2"/>'
+                    ].join('');
+                    var $node = utils.initWithNode(nodes, domManager).filter('#x');
+
+                    var spy = utils.spyOnNode($node);
+                    $node.prop('checked', true);
+                    $node.trigger('change');
+
+                    spy.getCall(0).args[1].should.eql({stuff: '8'});
+                });
+            });
+            describe('On UnCheck', function () {
+                it('should pass the right value on uncheck', function () {
+                    var nodes = [
+                        '<input type="radio" name="a" id="x" data-f-bind="stuff" value="8" checked/>',
+                        '<input type="radio" name="a" id="y" data-f-bind="stuff" value="2"/>'
+                    ].join('');
+                    var $node = utils.initWithNode(nodes, domManager).filter('#x');
+                    var $othernode = utils.initWithNode(nodes, domManager).filter('#y');
+
+                    //not entirely sure this is simulating well enough
+                    var spy = utils.spyOnNode($othernode);
+                    $node.prop('checked', false);
+                    $othernode.prop('checked', true);
+
+                    $othernode.trigger('change');
+
+                    spy.getCall(0).args[1].should.eql({stuff: '2'});
+                });
+            });
+        });
+    });
+}());
--- tests/testing-utils.js
+++ tests/testing-utils.js
@@ -3,7 +3,7 @@ var utils = {
     create: function (str) {
         var div = document.createElement('div');
         div.innerHTML = str;
-        return div.childNodes[0];
+        return (div.childNodes.length === 1) ? div.childNodes[0] : div.childNodes;
     },
     createDummyChannel: function () {
         var dummyChannel = {
@@ -22,14 +22,17 @@ var utils = {
 
     initWithNode: function(str, domManager) {
         var $node = $(utils.create(str));
+
         domManager.initialize({
             root: $node,
             channel: utils.createDummyChannel()
         });
         return $node;
     },
-    spyOnNode: function ($node) {
-        var spy = sinon.spy();
+    spyOnNode: function ($node, spy) {
+        if (!spy) {
+            spy = sinon.spy();
+        }
         $node.on('update.f.ui', function(){
             //sinon doesn't like passing the spy directly with 'this' as context.
             spy.apply(null, arguments);
