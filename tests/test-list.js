6dcaabce6155e2cdd75977f966de17b66d8ff55c
Naren Ranjit <narendran.ranjit@gmail.com>
2014-08-01 18:22:45 -0700

    moved attribute tests to dom

--- tests/test-list.js
+++ tests/test-list.js
@@ -5,6 +5,6 @@
 
 require('./specs/dom/test-dom-manager');
 require('./specs/converters/test-converter-manager');
-require('./specs/attributes/test-attribute-manager');
+require('./specs/dom/attributes/test-attribute-manager');
 require('./specs/dom/test-default-input-node');
 require('./specs/test-variables-channel');
