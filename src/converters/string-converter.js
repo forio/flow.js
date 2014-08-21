b4148d9629264ee9f1b2e33a7f670b3a1a7989ad
Naren Ranjit <narendran.ranjit@gmail.com>
2014-08-21 18:30:50 -0400

    Merged title-case converter into string converters

--- src/converters/string-converter.js
+++ src/converters/string-converter.js
@@ -1,7 +1,17 @@
 'use strict';
 module.exports = {
-    alias: 's',
-    convert: function (value) {
-        return value + '';
+    s: function (val) {
+        return val + '';
+    },
+
+    upperCase: function (val) {
+        return (val + '').toUpperCase();
+    },
+    lowerCase: function (val) {
+        return (val + '').toLowerCase();
+    },
+    titleCase: function (val) {
+        val = val + '';
+        return val.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
 };
--- src/converters/title-case-converter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = {
-    alias: 'titleCase',
-    convert: function (str) {
-        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    }
-};
