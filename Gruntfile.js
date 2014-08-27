721c6565378c4c4d39ca90adad0f88c03d86cdff
Naren Ranjit <narendran.ranjit@gmail.com>
2014-08-27 16:47:48 -0700

    added increment version to release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -167,7 +167,7 @@ module.exports = function(grunt) {
     grunt.registerTask('release', function (type) {
         //TODO: Integrate 'changelog' in here when it's stable
         type = type ? type : 'patch';
-        ['validate', 'production', 'bump-only:' + type, 'bump-commit'].forEach(function (task) {
+        ['validate', 'production', 'bump-only:' + type, 'incrementVersion', 'bump-commit'].forEach(function (task) {
             grunt.task.run(task);
         });
     });
