20e53ae2846de1be34e5612862d89bf6e4d40434
Naren Ranjit <narendran.ranjit@gmail.com>
2014-09-23 18:45:23 -0700

    test class attr with arrays

--- tests/specs/dom/attributes/test-class-attr.js
+++ tests/specs/dom/attributes/test-class-attr.js
@@ -4,6 +4,7 @@ module.exports = (function() {
     var utils = require('../../../testing-utils');
 
     describe('Classes', function () {
+
         it('should able to add classes if there are none', function () {
             var $node = utils.initWithNode('<input type="text" data-f-class="apple" data-f-bind="stuff"/>', domManager);
 
@@ -29,6 +30,20 @@ module.exports = (function() {
             $node.hasClass('sauce').should.equal(false);
             $node.hasClass('exist').should.equal(true);
             $node.hasClass('pie').should.equal(true);
+        });
+
+        it('should prefix value- if passed in a number', function () {
+            var $node = utils.initWithNode('<input type="text" data-f-class="data" data-f-bind="stuff"/>', domManager);
+
+            $node.trigger('update.f.model', {data: 2});
+            $node.hasClass('value-2').should.equal(true);
+        });
+
+        it('should use the last item if it\'s an array', function () {
+            var $node = utils.initWithNode('<input type="text" data-f-class="data" data-f-bind="stuff"/>', domManager);
+
+            $node.trigger('update.f.model', {data: [1,2,3]});
+            $node.hasClass('value-3').should.equal(true);
         });
     });
 }());
