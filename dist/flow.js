4013dec1fda289166e9220f78ad2f8f51486bae4
Naren Ranjit <narendran.ranjit@gmail.com>
2014-09-25 14:38:57 -0700

    append logo to generated files

--- dist/flow.js
+++ dist/flow.js
@@ -1,6 +1,29 @@
+/**
+
+++++++++   ++++++++   ++++++++           Flow.js
+++++++++   ,+++++++~   ++++++++          v0.6.3
+ ++++++++   ++++++++   ++++++++
+ ~+++++++~   ++++++++   ++++++++         Github: https://github.com/forio/flow.js
+  ++++++++   ++++++++   ++++++++:
+   :++++++++   ++++++++   ++++++++
+     ++++++++   =+++++++   ,+++++++=
+      ++++++++   ++++++++   ++++++++
+      =+++++++    +++++++=   ++++++++
+       ++++++++   ++++++++   ++++++++
+       ,+++++++:   +++++++~   ++++++++
+        +++++++=   +++++++=   ++++++++
+       ++++++++   ++++++++   ++++++++
+       ++++++++   ++++++++   ++++++++
+       ++++++++   =+++++++   :+++++++,
+        ++++++++   ++++++++   ++++++++
+        ++++++++   ~+++++++    +++++++=
+         ++++++++   ++++++++   ++++++++
+
+**/
+
 ;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 window.Flow = require('./flow.js');
-window.Flow.version = '0.6.3'; //populated by grunt
+window.Flow.version = '<%= version %>'; //populated by grunt
 
 },{"./flow.js":2}],2:[function(require,module,exports){
 'use strict';
