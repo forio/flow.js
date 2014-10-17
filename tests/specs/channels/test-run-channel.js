280e1630512e8079672c673911ed520825152983
Naren Ranjit <narendran.ranjit@gmail.com>
2014-10-17 08:57:04 -0700

    passing run options test

--- tests/specs/channels/test-run-channel.js
+++ tests/specs/channels/test-run-channel.js
@@ -8,24 +8,32 @@
             it('should pass options to the variables service', function () {
                 var options = {
                     refresh: {
-                        on: 'stuff'
+                        on: 'stuff',
+                        except: ['otherStuff']
                     }
                 };
                 var c = new Channel({
                     run: { variables: options }
                 });
-                c.variables.private.options.should.eql(options);
+                c.variables.private.options.refresh.should.eql({
+                        on: 'stuff',
+                        except: ['otherStuff']
+                    });
             });
             it('should pass options to the operations service', function () {
                 var options = {
                     refresh: {
-                        on: 'stuff'
+                        on: 'stuff',
+                        except: ['otherStuff']
                     }
                 };
                 var c = new Channel({
                     run: {operations: options}
                 });
-                c.operations.private.options.should.eql(options);
+                c.operations.private.options.refresh.should.eql({
+                        on: 'stuff',
+                        except: ['otherStuff']
+                    });
             });
         });
     });
