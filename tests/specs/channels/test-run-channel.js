8278165d02bb3a5c33ce2c9d813dddcbcd11740f
Naren Ranjit <narendran.ranjit@gmail.com>
2014-10-17 10:31:16 -0700

    slight test refactor

--- tests/specs/channels/test-run-channel.js
+++ tests/specs/channels/test-run-channel.js
@@ -3,37 +3,29 @@
 
     var Channel = require('../../../src/channels/run-channel');
     describe('Run Channel', function () {
+        var refreshOptions = {
+            on: 'stuff',
+            except: ['otherStuff']
+        };
         describe('options', function () {
 
             it('should pass options to the variables service', function () {
                 var options = {
-                    refresh: {
-                        on: 'stuff',
-                        except: ['otherStuff']
-                    }
+                    refresh: refreshOptions
                 };
                 var c = new Channel({
                     run: { variables: options }
                 });
-                c.variables.private.options.refresh.should.eql({
-                        on: 'stuff',
-                        except: ['otherStuff']
-                    });
+                c.variables.private.options.refresh.should.eql(refreshOptions);
             });
             it('should pass options to the operations service', function () {
                 var options = {
-                    refresh: {
-                        on: 'stuff',
-                        except: ['otherStuff']
-                    }
+                    refresh: refreshOptions
                 };
                 var c = new Channel({
                     run: {operations: options}
                 });
-                c.operations.private.options.refresh.should.eql({
-                        on: 'stuff',
-                        except: ['otherStuff']
-                    });
+                c.operations.private.options.refresh.should.eql(refreshOptions);
             });
         });
     });
