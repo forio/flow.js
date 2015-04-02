46795aa32fda508e28f4c935fecd1059c2606975
Naren Ranjit <narendran.ranjit@gmail.com>
2015-04-02 11:54:52 -0700

    changed coverage folder

--- grunt/coverage-report.js
+++ grunt/coverage-report.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
         function () {
             var collector = new istanbul.Collector();
             var reporter = new istanbul.Reporter(false, 'coverage');
-            collector.add(JSON.parse(fs.readFileSync('coverage.json', 'utf8')));
+            collector.add(JSON.parse(fs.readFileSync('coverage/coverage.json', 'utf8')));
             reporter.add('html');
             reporter.write(collector, true, function () {
                 //empty callback so it doesnt error out
