3fcc0c8bcf8847219523c96292e1d55bd9134239
Naren Ranjit <narendran.ranjit@gmail.com>
2015-04-10 08:12:42 -0700

    passing dom integration tests

--- tests/specs/dom/attributes/test-default-foreach-attr.js
+++ tests/specs/dom/attributes/test-default-foreach-attr.js
@@ -1,37 +1,56 @@
 module.exports = (function () {
     'use strict';
-    // var domManager = require('src/dom/dom-manager');
-    // var utils = require('../../../testing-utils');
+    var domManager = require('src/dom/dom-manager');
+    var utils = require('../../../testing-utils');
 
     var foreachHandler = require('src/dom/attributes/foreach/default-foreach-attr');
 
     describe('Default Foreach', function () {
-        describe('Arrays', function () {
-            it('should clone children for arrays', function () {
-                var $rootNode = $('<ul> <li> </li> </ul>');
+        describe('#handle', function () {
+            describe('Arrays', function () {
+                it('should clone children for arrays', function () {
+                    var $rootNode = $('<ul> <li> </li> </ul>');
 
-                foreachHandler.handle.call($rootNode, [1,2,3,4]);
-                var newChildren = $rootNode.children();
-                newChildren.length.should.equal(4);
-            });
-            it('should replace templated data attributes for children', function () {
-                var $rootNode = $('<ul> <li data-stuff="<%=value%>" data-y="4"> </li> </ul>');
-                var targetData = [5,3,6,1];
+                    foreachHandler.handle.call($rootNode, [1,2,3,4]);
+                    var newChildren = $rootNode.children();
+                    newChildren.length.should.equal(4);
+                });
+                it('should replace templated data attributes for children', function () {
+                    var $rootNode = $('<ul> <li data-stuff="<%=value%>"> </li> </ul>');
+                    var targetData = [5,3,6,1];
 
-                foreachHandler.handle.call($rootNode, targetData);
-                var newChildren = $rootNode.children();
-                newChildren.each(function (index) {
-                   var data = $(this).data('stuff');
-                   data.should.equal(targetData[index] + '');
+                    foreachHandler.handle.call($rootNode, targetData);
+                    var newChildren = $rootNode.children();
+                    newChildren.each(function (index) {
+                       var data = $(this).data('stuff');
+                       data.should.equal(targetData[index] + '');
+                    });
+                });
+
+                it('should replace templated inner html for children', function () {
+                    var $rootNode = $('<ul> <li data-stuff="<%=index%>"> <%= value %> </li> </ul>');
+                    var targetData = [5,3,6,1];
+
+                    foreachHandler.handle.call($rootNode, targetData);
+                    var newChildren = $rootNode.children();
+                    newChildren.each(function (index) {
+                       var data = $(this).html().trim();
+                       data.should.equal(targetData[index] + '');
+
+                       var indexVal = $(this).data('stuff');
+                       indexVal.should.equal(index + '');
+                    });
                 });
             });
-
-            it('should replace templated inner html for children', function () {
-                var $rootNode = $('<ul> <li data-stuff="<%=index%>" data-y="4"> <%= value %> </li> </ul>');
+        });
+        describe('integration', function () {
+            it('should loop through children for elems with foreach=variable', function () {
                 var targetData = [5,3,6,1];
 
-                foreachHandler.handle.call($rootNode, targetData);
-                var newChildren = $rootNode.children();
+                var $node = utils.initWithNode('<ul data-f-foreach="somearray"> <li data-stuff="<%=index%>"> <%= value %> </li> </ul>', domManager);
+                $node.trigger('update.f.model', { somearray: targetData });
+
+                var newChildren = $node.children();
                 newChildren.each(function (index) {
                    var data = $(this).html().trim();
                    data.should.equal(targetData[index] + '');
