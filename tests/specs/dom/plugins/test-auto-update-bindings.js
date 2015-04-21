884ec3af29f8727431a0772738cba017b8a85c9d
Naren Ranjit <narendran.ranjit@gmail.com>
2015-04-20 17:57:08 -0700

    update binding test

---
+++ tests/specs/dom/plugins/test-auto-update-bindings.js
@@ -0,0 +1,29 @@
+module.exports = (function () {
+    'use strict';
+
+    //FIXME: Can't really test this till phantomjs is updated to 2.0
+
+    var autoUpdate = require('src/dom/plugins/auto-update-bindings');
+    describe('Update behavior', function () {
+        it.skip('should call bind for added elements', function () {
+            var $node = $('<div> </div>');
+
+            var dummyDom = {
+                bindAll: sinon.spy(),
+                unbindAll: sinon.spy()
+            };
+
+            autoUpdate($node.get(0), dummyDom);
+            var $sampleNode1 = $('<input type="text" data-f-bind="stuff" />');
+            $node.append($sampleNode1);
+
+            dummyDom.bindAll.should.have.been.calledOnce;
+            dummyDom.bindAll.should.have.been.calledWith([$sampleNode1.get(0)]);
+        });
+
+        it('should call unbind for removed elements', function () {
+
+        });
+
+    });
+}());
