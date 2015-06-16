f26cdb1541a6bd702e4d639a4cbe717dc7f455f9
Molly Jones <mjones@forio.com>
2015-06-16 14:13:19 -0700

    epicenter-902 checkbox and input bind

--- src/dom/attributes/binds/input-bind-attr.js
+++ src/dom/attributes/binds/input-bind-attr.js
@@ -1,9 +1,21 @@
 /**
  * ## Inputs and Selects
  *
- * Special handling for DOM elements `input` and `select`. 
+ * In the [default case](../default-bind-attr/), the `data-f-bind` attribute creates a bi-directional binding between the DOM element and the model variable. This binding is **bi-directional**, meaning that as the model changes, the interface is automatically updated; and when end users change values in the interface, the model is automatically updated.
  *
- * TODO -- is this for: "<select data-f-bind='x'>" NOT  for "<select><option data-f-selected='x'>" ? 
+ * Flow.js provides special handling for DOM elements `input` and `select`. 
+ *
+ * In particular, if you add the `data-f-bind` attribute to a `select` or `input` element, the option matching the value of the model variable is automatically selected.
+ *
+ * **Example**
+ *
+ * 		<!-- option selected if sample_int is 8, 10, or 12 -->
+ * 		<select data-f-bind="sample_int">
+ * 			<option value="8"> 8 </option>
+ * 			<option value="10"> 10 </option>
+ * 			<option value="12"> 12 </option>
+ * 		</select>
+ *
  */
 
 'use strict';
