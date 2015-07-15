66a0a9e01e81c07ad068aafbecbc3b1d8638035f
Molly Jones <mjones@forio.com>
2015-07-15 15:24:15 -0700

    fix browserify to match master

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
                 preBundleCB: function (b) {
                     b.plugin(remapify, {
                         src: '**/*.js',
-                        cwd: './instrument/',
+                        cwd: './src/',
                         expose: 'src'
                     });
                 },
