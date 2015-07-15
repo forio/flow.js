a62afe2b27907800337b6a436d2947ac24a613ba
Molly Jones <mjones@forio.com>
2015-07-15 16:07:22 -0700

    clean up comments in markdox file

--- grunt/markdox.js
+++ grunt/markdox.js
@@ -9,13 +9,6 @@ module.exports = function (grunt) {
         },
         target: {
             files: [
-
-                // channels
-                /* {
-                    // no publicAPI yet for run-channel, so don't build docs
-                    src: 'src/channels/run-channel.js',
-                    dest: 'documentation/generated/run-channel/index.html.md'
-                }, */
                 {
                     src: 'src/channels/operations-channel.js',
                     dest: 'documentation/generated/channels/operations-channel/index.html.md'
@@ -23,9 +16,6 @@ module.exports = function (grunt) {
                     src: 'src/channels/variables-channel.js',
                     dest: 'documentation/generated/channels/variables-channel/index.html.md'
                 },
-
-
-                // TODO - decide if converters should all go in one file, or if this is ok
                 {
                     src: 'src/converters/array-converter.js',
                     dest: 'documentation/generated/converters/array-converter/index.html.md'
@@ -42,21 +32,10 @@ module.exports = function (grunt) {
                     src: 'src/converters/string-converter.js',
                     dest: 'documentation/generated/converters/string-converter/index.html.md'
                 },
-
-
-                // dom manager goes at root level of /generated/dom for now
                 {
                     src: 'src/dom/dom-manager.js',
                     dest: 'documentation/generated/dom/index.html.md'
                 },
-                // dom/nodes
-                // TODO ask Naren if other files here make sense for docs yet
-                {
-                    src: 'src/dom/nodes/node-manager.js',
-                    dest: 'documentation/generated/dom/nodes/node-manager/index.html.md'
-                },
-                // dom/attributes
-                // TODO ask Naren if other files at dom/attributes make sense for docs yet, i think no
                 {
                     src: 'src/dom/attributes/attribute-manager.js',
                     dest: 'documentation/generated/dom/attributes/attribute-manager/index.html.md'
@@ -73,10 +52,6 @@ module.exports = function (grunt) {
                     src: 'src/dom/attributes/positive-boolean-attr.js',
                     dest: 'documentation/generated/dom/attributes/boolean-attr/index.html.md'
                 },
-
-
-                // dom/attributes/binds/
-                // TODO ask Naren if other files (e.g. webcomponents) at dom/attributes make sense for docs yet, i think no
                 {
                     src: 'src/dom/attributes/binds/checkbox-radio-bind-attr.js',
                     dest: 'documentation/generated/dom/attributes/binds/checkbox-radio-bind-attr/index.html.md'
@@ -87,7 +62,6 @@ module.exports = function (grunt) {
                     src: 'src/dom/attributes/binds/input-bind-attr.js',
                     dest: 'documentation/generated/dom/attributes/binds/input-bind-attr/index.html.md'
                 },
-                // dom/attributes/events
                 {
                     src: 'src/dom/attributes/events/default-event-attr.js',
                     dest: 'documentation/generated/dom/attributes/events/default-event-attr/index.html.md'
@@ -95,13 +69,10 @@ module.exports = function (grunt) {
                     src: 'src/dom/attributes/events/init-event-attr.js',
                     dest: 'documentation/generated/dom/attributes/events/init-event-attr/index.html.md'
                 },
-
-                // dom/attributes/foreach
                 {
                     src: 'src/dom/attributes/foreach/default-foreach-attr.js',
                     dest: 'documentation/generated/dom/attributes/foreach/default-foreach-attr/index.html.md'
                 },
-
                 // flow.js initialize
                 {
                     src: 'src/flow.js',
