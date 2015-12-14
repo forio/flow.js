541cb9facdf27f6a457491d1109841fb5d683007
Naren Ranjit <narendran.ranjit@gmail.com>
2015-12-14 11:35:12 -0800

    demo prep

--- src/add-ons/flow-inspector/flow-inspector.js
+++ src/add-ons/flow-inspector/flow-inspector.js
@@ -51,14 +51,20 @@ var FlowInspector = function (root) {
     };
 
 
-    var config = Flow.channel.run.getCurrentConfig();
-    config.token = 'eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI3YmM5MTI3My0yOGQzLTQyYWUtYTFhMy05YzEwYzg3ZmM0NTMiLCJzdWIiOiIwOTdlOTMzMC1mOWYyLTQ0MTAtYTY3My0wYTQwNzkyZjFkMTgiLCJzY29wZSI6WyJvYXV0aC5hcHByb3ZhbHMiLCJvcGVuaWQiXSwiY2xpZW50X2lkIjoibG9naW4iLCJjaWQiOiJsb2dpbiIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfaWQiOiIwOTdlOTMzMC1mOWYyLTQ0MTAtYTY3My0wYTQwNzkyZjFkMTgiLCJ1c2VyX25hbWUiOiJucmFuaml0QGZvcmlvLmNvbSIsImVtYWlsIjoibnJhbmppdEBmb3Jpby5jb20iLCJwYXJlbnRfYWNjb3VudF9pZCI6bnVsbCwiaWF0IjoxNDQ5ODU3ODMwLCJleHAiOjE0NDk5MDEwMzAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6OTc2My91YWEvb2F1dGgvdG9rZW4iLCJhdWQiOlsib2F1dGgiLCJvcGVuaWQiXX0.iE2FccE3UqWBSmTQxraz0Isyb5w17aki1Q_BY_yM0qvdVLxIPq17oCtY3u8cL2uqLeIqvyvH7WRUvccadbh1fOisPX-V2NyfJg0sV82ti_yy4McDW-7ja8ilwP1QDXzsCOlMs4xyfCnd9KyJ7DiwYbrK2uhMvxV3NtFaH3WbrDLptf128zl3olLHN3XuyB0gvXHnfeitgDh-J79Kl7gC8JkGNK5_q--FddjJFVQz-dTwsPMzGQt-Tro7Hu45ACyn07B4IaFqIggmOtR9cUD7pao_YNzgMetLQ5mMz3ExIasWciqQ8E32S6S6s_Jp_3CpyZdCwlUWumI8yeZTsS9TAg';
-    var file = new F.service.File(config);
-    var promise = file.getFileContents(config.model, 'model').then(function (x) {
-        var ret = x.split(/\n/);
-        // return _.invoke(ret, 'trim');
-        return ret;
-    });
+    var promise;
+    if (window.Flow.channel.run.getCurrentConfig) {
+        var config = window.Flow.channel.run.getCurrentConfig();
+        var modelType = config.model.split('.')[1];
+        if (modelType !== 'vmf') {
+            config.token = 'eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI3YmM5MTI3My0yOGQzLTQyYWUtYTFhMy05YzEwYzg3ZmM0NTMiLCJzdWIiOiIwOTdlOTMzMC1mOWYyLTQ0MTAtYTY3My0wYTQwNzkyZjFkMTgiLCJzY29wZSI6WyJvYXV0aC5hcHByb3ZhbHMiLCJvcGVuaWQiXSwiY2xpZW50X2lkIjoibG9naW4iLCJjaWQiOiJsb2dpbiIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInVzZXJfaWQiOiIwOTdlOTMzMC1mOWYyLTQ0MTAtYTY3My0wYTQwNzkyZjFkMTgiLCJ1c2VyX25hbWUiOiJucmFuaml0QGZvcmlvLmNvbSIsImVtYWlsIjoibnJhbmppdEBmb3Jpby5jb20iLCJwYXJlbnRfYWNjb3VudF9pZCI6bnVsbCwiaWF0IjoxNDQ5ODU3ODMwLCJleHAiOjE0NDk5MDEwMzAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6OTc2My91YWEvb2F1dGgvdG9rZW4iLCJhdWQiOlsib2F1dGgiLCJvcGVuaWQiXX0.iE2FccE3UqWBSmTQxraz0Isyb5w17aki1Q_BY_yM0qvdVLxIPq17oCtY3u8cL2uqLeIqvyvH7WRUvccadbh1fOisPX-V2NyfJg0sV82ti_yy4McDW-7ja8ilwP1QDXzsCOlMs4xyfCnd9KyJ7DiwYbrK2uhMvxV3NtFaH3WbrDLptf128zl3olLHN3XuyB0gvXHnfeitgDh-J79Kl7gC8JkGNK5_q--FddjJFVQz-dTwsPMzGQt-Tro7Hu45ACyn07B4IaFqIggmOtR9cUD7pao_YNzgMetLQ5mMz3ExIasWciqQ8E32S6S6s_Jp_3CpyZdCwlUWumI8yeZTsS9TAg';
+            var file = new F.service.File(config);
+            promise = file.getFileContents(config.model, 'model').then(function (x) {
+                var ret = x.split(/\n/);
+                // return _.invoke(ret, 'trim');
+                return ret;
+            });
+        }
+    }
 
     var findContext = function (codeArray, variable, isFunction) {
         variable = variable.split(/[\[\.\(]/)[0];
@@ -81,7 +87,7 @@ var FlowInspector = function (root) {
                 if (modelType === 'jl') {
                     return val.trim() === 'end';
                 } else if (modelType === 'py') {
-                    val.trim() === '';
+                    return val.trim() === '';
                 }
             } else {
                 return val.trim() === '' || (val.indexOf('=') !== -1);
@@ -151,25 +157,26 @@ var FlowInspector = function (root) {
                 }
             });
 
-            $thisElemContainer.on('click', '.f-bind, .f-foreach', function (evt) {
-                var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
-                var variableName = $target.text().trim();
-                promise.then(function (data) {
-                    var context = findContext(data, variableName);
-                    $codePanel.find('pre').html(context);
+            if (promise) {
+                $thisElemContainer.on('click', '.f-bind, .f-foreach', function (evt) {
+                    var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
+                    var variableName = $target.text().trim();
+                    promise.then(function (data) {
+                        var context = findContext(data, variableName);
+                        $codePanel.find('pre').html(context);
+                    });
                 });
-            });
 
-            $thisElemContainer.on('click', '.f-on', function (evt) {
-                var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
-                var variableName = $target.text().trim();
-                promise.then(function (data) {
-                    var context = findContext(data, variableName, true);
+                $thisElemContainer.on('click', '.f-on', function (evt) {
+                    var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
+                    var variableName = $target.text().trim();
+                    promise.then(function (data) {
+                        var context = findContext(data, variableName, true);
 
-                    $codePanel.find('pre').html(context);
+                        $codePanel.find('pre').html(context);
+                    });
                 });
-            });
-
+            }
 
             $overlayContainer.append($thisElemContainer);
         });
@@ -182,7 +189,9 @@ var FlowInspector = function (root) {
 
 
         $(root).prepend($overlayContainer);
-        $overlayContainer.append($codePanel);
+        if (promise) {
+            $overlayContainer.append($codePanel);
+        }
     });
 
 
