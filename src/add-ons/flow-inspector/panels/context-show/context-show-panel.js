c51a30b25f66c2eac562c625954e503cb5f4d5cf
Naren Ranjit <narendran.ranjit@gmail.com>
2015-12-14 17:15:01 -0800

    handle not authenticated case

--- src/add-ons/flow-inspector/panels/context-show/context-show-panel.js
+++ src/add-ons/flow-inspector/panels/context-show/context-show-panel.js
@@ -7,33 +7,33 @@ module.exports = function ($container) {
     var $html = $(template);
 
     if (!window.Flow.channel.run.getCurrentConfig) {
-        console.warn('You\'re using an older version of FlowJS which does not support viewing model context');
+        console.warn('You\'re using an older version of FlowJS which does not support viewing model context.');
         return false;
     }
 
     var config = window.Flow.channel.run.getCurrentConfig();
     var modelType = config.model.split('.')[1];
     if (modelType === 'vmf') {
-        console.warn('Cannot view context for Vensim models');
+        console.info('Cannot view context for Vensim models.');
         return false;
     }
 
     var file = new F.service.File(config);
-    var promise = file.getFileContents(config.model, 'model').then(function (response) {
-        var extractor = new ContextExtractor(config.model, response);
-        return extractor;
-    });
-    $container.on('click', '.f-bind, .f-foreach, .f-on', function (evt) {
-        var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
-        var variableName = $target.text().trim();
-        var isFunction = $target.parent().is('.f-on');
-        promise.then(function (extractor) {
-            var context = extractor.showContext(variableName, isFunction);
-            $html.find('pre').html(context);
+    file.getFileContents(config.model, 'model')
+        .then(function (response) {
+            var extractor = new ContextExtractor(config.model, response);
+            $container.on('click', '.f-bind, .f-foreach, .f-on', function (evt) {
+                var $target = $(evt.target).is('.f-val') ? $(evt.target) : $(evt.target).parent().find('.f-val');
+                var variableName = $target.text().trim();
+                var isFunction = $target.parent().is('.f-on');
+                var context = extractor.showContext(variableName, isFunction);
+                $html.find('pre').html(context);
+            });
+            $container.append($html);
+        })
+        .fail(function () {
+            console.info('Could not get model file contents; this is only available if you\'re logged in as an admin or team-member.');
         });
-    });
-
-    $container.append($html);
 };
 
 
