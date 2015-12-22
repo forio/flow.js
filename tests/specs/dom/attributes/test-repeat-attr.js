b2582eb10e50b1ae28b4133b5f98d78be469f96c
Naren Ranjit <narendran.ranjit@gmail.com>
2015-12-22 12:50:17 -0800

    test nested repeats

--- tests/specs/dom/attributes/test-repeat-attr.js
+++ tests/specs/dom/attributes/test-repeat-attr.js
@@ -169,6 +169,45 @@ module.exports = (function () {
                 });
             });
         });
+        describe('Parallel repeats', function () {
+            it('should not affect siblings on first render', function () {
+                var $rootNode = $('<ul> <li class="first"> </li> <li class="second"> </li> </ul>');
+
+                repeatHandler.handle.call($rootNode.find('li.first'), [1,2,3,4]);
+                repeatHandler.handle.call($rootNode.find('li.second'), ['a', 'b', 'c', 'd']);
+                var newChildren = $rootNode.children();
+                newChildren.length.should.equal(8);
+            });
+
+            it('should not affect siblings on update', function () {
+                var $rootNode = $('<ul> <li class="first"> </li> <li class="second"> </li> </ul>');
+
+                repeatHandler.handle.call($rootNode.find('li.first'), [1,2,3,4]);
+                repeatHandler.handle.call($rootNode.find('li.second'), ['a', 'b', 'c', 'd']);
+                var newChildren = $rootNode.children();
+                newChildren.length.should.equal(8);
+
+                var opdata = [1,2,3,4,5,6, 'a', 'b', 'c' , 'd'];
+                repeatHandler.handle.call($rootNode.find('li.first'), [1,2,3,4,5,6]);
+                newChildren = $rootNode.children();
+                newChildren.each(function (index) {
+                   var data = $(this).html().trim();
+                   data.should.equal(opdata[index] + '');
+                });
+
+            });
+        });
+        describe('Nested Repeats', function () {
+            it('should not affect children', function () {
+                var $rootNode = $('<ul> <li class="first"> <div class="second"> </div> </li> </ul>');
+
+                repeatHandler.handle.call($rootNode.find('.first'), [1,2,3,4]);
+                repeatHandler.handle.call($rootNode.find('.second'), ['a', 'b', 'c', 'd']);
+                var newChildren = $rootNode.children();
+                newChildren.length.should.equal(4);
+                $rootNode.find('.second').length.should.equal(4 * 4);
+            });
+        });
         describe('integration', function () {
             it('should loop through children for elems with repeat=variableArray', function () {
                 var targetData = [5,3,6,1];
@@ -209,23 +248,26 @@ module.exports = (function () {
                 $node.find('li:first').trigger('update.f.model', { someobject: targetData });
                 $node.children().length.should.equal(childrenCount);
             });
-            it('should support inline functions in templates', function () {
+            it('should support nested repeats', function () {
                 var targetData = [5,3,6,1];
-                var outputdata = ['first',3,6,1];
+                var targetData2 = ['a', 'b', 'c'];
 
-                var $node = utils.initWithNode('<ul> <li data-f-repeat="somearray"> <%= (index === 0) ? "first" : value %> <%= value %></li> </ul>', domManager);
+                var $node = utils.initWithNode('<ul> <li data-f-repeat="somearray"> <div class="children" data-f-repeat="somethingElse"> </div> </li> </ul>', domManager);
                 $node.find('li:first').trigger('update.f.model', { somearray: targetData });
+
+                domManager.bindAll();
+
+                $node.find('div').trigger('update.f.model', { somethingElse: targetData2 });
 
                 var newChildren = $node.children();
-                var childrenCount = newChildren.size();
-                newChildren.each(function (index) {
-                   var data = $(this).html().trim();
-                   data.should.equal(outputdata[index] + ' ' + targetData[index]);
+                newChildren.length.should.equal(targetData.length);
+                newChildren.each(function (index, el) {
+                    $(el).children().length.should.equal(targetData2.length);
+                    $(el).children().each(function (i2, el2) {
+                        var data = $(this).html().trim();
+                        data.should.equal(targetData2[i2]);
+                    });
                 });
-
-                $node.find('li:first').trigger('update.f.model', { somearray: targetData });
-                $node.children().length.should.equal(childrenCount);
-
             });
         });
     });
