b68e90278324c0fb69c6d5f2c3a516799c614547
Naren Ranjit <narendran.ranjit@gmail.com>
2016-04-20 10:16:51 -0700

    comment attr usage

--- src/config.js
+++ src/config.js
@@ -17,14 +17,19 @@ module.exports = {
         //Subscription id returned by the channel. Used to ubsubscribe later
         subscriptionId: 'f-subscription-id',
 
+        //Used by the classes attr handler to keep track of which classes were added by itself
         classesAdded: 'f-added-classes',
 
+        //Used by repeat attr handler to keep track of template after first evaluation
         repeat: {
             template: 'f-repeat-template',
             templateId: 'f-repeat-template-id'
         },
 
+        //Used by foreach attr handler to keep track of template after first evaluation
         foreachTemplate: 'f-foreach-template',
+
+        //Used by bind attr handler to keep track of template after first evaluation
         bindTemplate: 'f-bind-template'
     }
 };
