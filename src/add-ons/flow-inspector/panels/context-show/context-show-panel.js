516e1cc8fb4ffc9b4c96a4415906b49095750f27
naren ranjit <narenranjit@Arcane.local>
2016-12-08 12:45:08 -0800

    pass in model to be backwards compatible with older epijs version

--- src/add-ons/flow-inspector/panels/context-show/context-show-panel.js
+++ src/add-ons/flow-inspector/panels/context-show/context-show-panel.js
@@ -29,7 +29,7 @@ module.exports = function ($container, configFunction) {
     };
     var opts = $.extend(true, {}, defaults, config);
     var file = new F.service.File(opts);
-    file.getContents(config.model)
+    file.getContents(config.model, 'model')
         .then(function (response) {
             var extractor = new ContextExtractor(config.model, response);
             $container.on('click', '.f-bind, .f-loop, .f-on', function (evt) {
