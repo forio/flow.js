f931a46907ae586baaa3c44b351bc2e373396853
Naren <narendran.ranjit@gmail.com>
2016-12-08 08:20:35 -0800

    leave log un-noisy by default

--- grunt/mocha.js
+++ grunt/mocha.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
                 growlOnSuccess: false,
                 reporter: 'Min',
                 run: true,
-                log: true,
+                // log: true,
             }
         }
     });
