cb8108c433b4d653fd37cb5e48b8b2e5c94d53d3
Naren <narendran.ranjit@gmail.com>
2016-12-08 01:27:15 -0800

    return prom

--- tests/specs/dom/element-tests/test-text.js
+++ tests/specs/dom/element-tests/test-text.js
@@ -6,7 +6,7 @@ module.exports = (function () {
     describe(':text', function () {
         describe('input handlers', function () {
             it('should trigger the right event on ui change', function () {
-                utils.initWithNode('<input type="text" data-f-bind="stuff"/>', domManager).then(function ($node) {
+                return utils.initWithNode('<input type="text" data-f-bind="stuff"/>', domManager).then(function ($node) {
                     var spy = utils.spyOnNode($node);
                     $node.trigger('change');
                     spy.should.have.been.called.once;
@@ -14,7 +14,7 @@ module.exports = (function () {
             });
 
             it('should pass the right params to the event', function () {
-                utils.initWithNode('<input type="text" data-f-bind="stuff" value="3"/>', domManager).then(function ($node) {
+                return utils.initWithNode('<input type="text" data-f-bind="stuff" value="3"/>', domManager).then(function ($node) {
                     var spy = utils.spyOnNode($node);
 
                     $node.val(5);
@@ -26,7 +26,7 @@ module.exports = (function () {
         });
         describe('updaters', function () {
             it('should update itself with values passed in', function () {
-                utils.initWithNode('<input type="text" data-f-bind="stuff" value="3"/>', domManager).then(function ($node) {
+                return utils.initWithNode('<input type="text" data-f-bind="stuff" value="3"/>', domManager).then(function ($node) {
                     $node.trigger('update.f.model', { stuff: 5 });
 
                     var val = $node.val();
