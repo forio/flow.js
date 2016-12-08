4d06a582627c5a6c7f984c22118cb0d29cd45939
Naren <narendran.ranjit@gmail.com>
2016-12-08 01:26:09 -0800

    return prom

--- tests/specs/dom/attributes/test-default-attr.js
+++ tests/specs/dom/attributes/test-default-attr.js
@@ -5,14 +5,14 @@ module.exports = (function () {
 
     describe('Default Attributes', function () {
         it('should copy attributes for anything it doesn\'t understand', function () {
-            utils.initWithNode('<input type="text" data-f-fruit="apple" data-f-bind="stuff"/>', domManager).then(function ($node) {
+            return utils.initWithNode('<input type="text" data-f-fruit="apple" data-f-bind="stuff"/>', domManager).then(function ($node) {
                 $node.trigger('update.f.model', { apple: 'sauce' });
                 $node.prop('fruit').should.equal('sauce');
             });
         });
 
         it('should copy attributes as arrays for arrays', function () {
-            utils.initWithNode('<input type="text" data-f-fruit="apple" data-f-bind="stuff"/>', domManager).then(function ($node) {
+            return utils.initWithNode('<input type="text" data-f-fruit="apple" data-f-bind="stuff"/>', domManager).then(function ($node) {
                 $node.trigger('update.f.model', { apple: [1, 2, 3] });
                 $node.prop('fruit').should.eql([1, 2, 3]);
             });
