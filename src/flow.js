bb6c1684c3b05e6e5d13a4f7c428938aba74ad4f
naren ranjit <narenranjit@Arcane.foriodev.com>
2016-12-09 11:26:33 -0800

    add back Flow.version

--- src/flow.js
+++ src/flow.js
@@ -122,5 +122,5 @@ var Flow = {
     }
 };
 //set by grunt
-// if (RELEASE_VERSION) Flow.version = RELEASE_VERSION; //eslint-disable-line no-undef
+if (RELEASE_VERSION) Flow.version = RELEASE_VERSION; //eslint-disable-line no-undef
 module.exports = Flow;
