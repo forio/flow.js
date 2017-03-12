c9f2455c515806ea2e752006271a3f08de6af225
Naren <narendran.ranjit@gmail.com>
2017-03-12 16:48:09 -0700

    working baseline

--- src/channels/middleware/epicenter-middleware/scenario-manager-router.js
+++ src/channels/middleware/epicenter-middleware/scenario-manager-router.js
@@ -15,33 +15,32 @@ export default function (config, notifier) {
     var baselinePromise = sm.baseline.getRun().then(function () {
         return sm.baseline.run;
     });
+    var baselineOptions = $.extend(true, {}, {
+        serviceOptions: baselinePromise,
+        channelOptions: {
+            meta: {
+                readOnly: true
+            },
+            variables: {
+                readOnly: true
+            }
+        }
+    }, opts.defaults, opts.baseline);
     var currentRunPromise = sm.current.getRun().then(function () {
         return sm.current.run;
     });
-    var baselineRunChannel = new RunChannel( 
-        $.extend(true, {}, {
-            serviceOptions: baselinePromise,
-            channelOptions: {
-                meta: {
-                    readOnly: true
-                },
-                variables: {
-                    readOnly: true
-                }
-            }
-        }, opts.defaults, opts.baseline)
-    , withPrefix(notifier, 'baseline'));
 
     var runOptions = $.extend(true, {}, {
         serviceOptions: currentRunPromise,
     }, opts.defaults, opts.current);
 
-    var currentRunChannel = new RunChannel(runOptions, withPrefix(notifier, 'current'));
+    var baselineRunChannel = new RunChannel(baselineOptions, withPrefix(notifier, 'baseline:'));
+    var currentRunChannel = new RunChannel(runOptions, withPrefix(notifier, 'current:'));
     var defaultRunChannel = new RunChannel(runOptions, withPrefix(notifier, ''));
 
     var handlers = [
-        $.extend(baselineRunChannel, { name: 'baseline', match: prefix('baseline') }),
-        $.extend(currentRunChannel, { name: 'current', match: prefix('current') }),
+        $.extend(baselineRunChannel, { name: 'baseline', match: prefix('baseline:') }),
+        $.extend(currentRunChannel, { name: 'current', match: prefix('current:') }),
         $.extend(defaultRunChannel, { name: 'default', match: prefix('') }),
     ];
     
