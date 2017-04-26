4ac9d8944b06c3822380cbd87f48f3f22f2e25d2
Naren <narendran.ranjit@gmail.com>
2017-04-26 13:09:38 -0700

    account for values being arrays

--- src/dom/attributes/toggles/hide-if-attr.js
+++ src/dom/attributes/toggles/hide-if-attr.js
@@ -6,6 +6,9 @@ module.exports = {
     target: '*',
 
     handle: function (value, prop) {
+        if (_.isArray(value)) {
+            value = value[value.length - 1];
+        }
         return value ? this.hide() : this.show();
     }
 };
--- src/dom/attributes/toggles/show-if-attr.js
+++ src/dom/attributes/toggles/show-if-attr.js
@@ -4,6 +4,9 @@ module.exports = {
     target: '*',
 
     handle: function (value, prop) {
+        if (_.isArray(value)) {
+            value = value[value.length - 1];
+        }
         return value ? this.show() : this.hide();
     }
 };
