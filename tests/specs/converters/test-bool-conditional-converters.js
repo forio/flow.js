9732ab6dfc6ed0e0cf288c83a907259baa350c3a
Naren <narendran.ranjit@gmail.com>
2017-04-26 12:49:52 -0700

    extra test for iftrue

--- tests/specs/converters/test-bool-conditional-converters.js
+++ tests/specs/converters/test-bool-conditional-converters.js
@@ -1,5 +1,7 @@
 'use strict';
 var nc = require('src/converters/bool-conditional-converters');
+var cm = require('src/converters/converter-manager.js');
+
 module.exports = (function () {
     describe('Boolean Conditional', function () {
         describe('ifTrue', function () {
@@ -20,6 +22,17 @@ module.exports = (function () {
             it('return false value if provided', function () {
                 nc.ifFalse('boo', 'baa', true).should.equal('baa');
                 nc.ifFalse('boo', 'baa', false).should.equal('boo');
+            });
+        });
+
+        describe('Integration', ()=> {
+            it('ifTrue: return true value if provided', function () {
+                cm.convert(true, 'ifTrue(boo)').should.equal('boo');
+                cm.convert(false, 'ifTrue(boo)').should.equal(false);
+            });
+            it('ifTrue: return false value if provided', function () {
+                cm.convert(true, 'ifTrue(boo, baa)').should.equal('boo');
+                cm.convert(false, 'ifTrue(boo, baa)').should.equal('baa');
             });
         });
     });
