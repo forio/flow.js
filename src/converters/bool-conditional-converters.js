0d0d186951c1ddd415a480a4f61d6595778dfc95
Molly Jones <mjones@forio.com>
2017-05-11 09:53:43 -0700

    boolean conditional converter docs

--- src/converters/bool-conditional-converters.js
+++ src/converters/bool-conditional-converters.js
@@ -1,3 +1,23 @@
+/**
+ * ## Boolean Conditional Converters
+ *
+ * Converters allow you to convert data -- in particular, model variables that you display in your project's user interface -- from one form to another.
+ *
+ * In particular, for a boolean conditional converter, the original format is your model variable, and the resulting "format" is a boolean value, or another value of your choosing.
+ *
+ * There are two ways to specify conversion or formatting for the display output of a particular model variable:
+ *
+ * * Use the `|` (pipe) character within the value of any `data-f-` attribute (not just `data-f-bind` or `data-f-foreach`).
+ * * Add the attribute `data-f-convert` to any element that also has the `data-f-bind` or `data-f-foreach`.
+ *
+ * For example:
+ *
+ *      <!-- will show "true" or "false" -->
+ *      <span data-f-bind="sampleVar | toBool"></span>
+ *      <span data-f-bind="sampleVar" data-f-convert="toBool"></span>
+ *
+ */
+
 'use strict';
 
 function parseArgs(trueVal, falseVal, input, matchString) {
@@ -14,17 +34,58 @@ function parseArgs(trueVal, falseVal, input, matchString) {
 
 module.exports = {
     /**
-     * Converts a 'truthy' values to true and 'falsy' values to false
+     * Convert a 'truthy' value to true and 'falsy' values to false.
+     *
+     * **Example**
+     *
+     *      <!-- displays "true" or "false" -->
+     *      <span data-f-bind="sampleDecisionVar | toBool"></span>
+     * 
      * @param {Any} value
      * @return {Boolean}
      */
     toBool: function (value) {
         return !!value;
     },
+
+    /**
+     * Convert the input to a new value (for example, some text), based on whether it is true or false.
+     *
+     * **Example**
+     *
+     *      <div>
+     *
+     *          <span data-f-bind="sampleDecisionVar | ifTrue('yes! please move forward', 'not ready to proceed')"></span> 
+     *
+     *          <span data-f-bind="sampleDecisionVar | ifTrue('yes! please move forward')"></span>
+     *      </div>
+     *
+     * @param {Number} trueVal The value to display if the input is true.
+     * @param {Number} falseVal (Optional) The value to display if the input is false. If not included, returns false.
+     * @param {Any} input (Optional) The input to test. If not included, the output of the previous argument is used.
+     * @return {Any} If input is true, returns trueVal. If input is false, returns falseVal if provided, or false.
+     */
     ifTrue: function () {
         var args = parseArgs.apply(null, arguments);
         return args.input ? args.trueVal : args.falseVal;
     },
+    /**
+     * Convert the input to a new value (for example, some text), based on whether it is false or true.
+     *
+     * **Example**
+     *
+     *      <div>
+     *
+     *          <span data-f-bind="sampleDecisionVar | ifFalse('not ready to proceed', 'actually this is still true')"></span> 
+     *
+     *          <span data-f-bind="sampleDecisionVar | ifFalse('not ready to proceed')"></span>
+     *      </div>
+     *
+     * @param {Number} trueVal The value to display if the input is false.
+     * @param {Number} falseVal (Optional) The value to display if the input is true. If not included, returns false.
+     * @param {Any} input (Optional) The input to test. If not included, the output of the previous argument is used.
+     * @return {Any} If input is false, returns trueVal. If input is true, returns falseVal if provided, or false.
+     */
     ifFalse: function () {
         var args = parseArgs.apply(null, arguments);
         return !args.input ? args.trueVal : args.falseVal;
