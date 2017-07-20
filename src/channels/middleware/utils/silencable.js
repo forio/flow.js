a5b02a56d3443f5813d15aca921ea870a06fe0a0
Naren <narendran.ranjit@gmail.com>
2017-07-20 12:16:39 -0700

    reduce -> filter

--- src/channels/middleware/utils/silencable.js
+++ src/channels/middleware/utils/silencable.js
@@ -9,19 +9,9 @@ export default function silencable(published, silentOptions) {
     if (silentOptions === true || !published) {
         return [];
     } else if (isArray(silentOptions)) {
-        return published.reduce((accum, data)=> {
-            if (!includes(silentOptions, data.name)) {
-                accum.push(data);
-            }
-            return accum;
-        }, []);
+        return published.filter((data) => !includes(silentOptions, data.name));
     } else if (silentOptions && silentOptions.except) {
-        return published.reduce((accum, data)=> {
-            if (includes(silentOptions.except || [], data.name)) {
-                accum.push(data);
-            }
-            return accum;
-        }, []);
+        return published.filter((data) => includes(silentOptions.except || [], data.name));
     }
     return published;
 }
