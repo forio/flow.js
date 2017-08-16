f80e58fdd9c2e2977ebb27eee3f535727a2e8a60
Naren <narendran.ranjit@gmail.com>
2017-08-16 12:35:22 -0700

    update parse utils tests

--- src/utils/parse-utils.js
+++ src/utils/parse-utils.js
@@ -1,43 +1,54 @@
 'use strict';
 
-module.exports = {
+function toImplicitType(data) {
+    var objRegex = /^(?:\{.*\})$/;
+    var arrRegex = /^(?:\[.*])$/;
 
-    toImplicitType: function (data) {
-        var rbrace = /^(?:\{.*\}|\[.*])$/;
-        var converted = data;
-        if (typeof data === 'string') {
-            data = data.trim();
+    var converted = data;
+    if (typeof data === 'string') {
+        data = data.trim();
 
-            if (data === 'true') {
-                converted = true;
-            } else if (data === 'false') {
-                converted = false;
-            } else if (data === 'null') {
-                converted = null;
-            } else if (data === 'undefined') {
-                converted = '';
-            } else if (converted.charAt(0) === '\'' || converted.charAt(0) === '"') {
-                converted = data.substring(1, data.length - 1);
-            } else if ($.isNumeric(data)) {
-                converted = +data;
-            } else if (rbrace.test(data)) {
-                //TODO: This only works with double quotes, i.e., [1,"2"] works but not [1,'2']
+        if (data === 'true') {
+            converted = true;
+        } else if (data === 'false') {
+            converted = false;
+        } else if (data === 'null') {
+            converted = null;
+        } else if (data === 'undefined') {
+            converted = '';
+        } else if (data.charAt(0) === '\'' || data.charAt(0) === '"') {
+            converted = data.substring(1, data.length - 1);
+        } else if ($.isNumeric(data)) {
+            converted = +data;
+        } else if (arrRegex.test(data)) {
+            const bracketReplaced = data.replace(/[[\]]/g, '');
+            if (!bracketReplaced) return [];
+            const parsed = bracketReplaced.split(/,(?![^[]*])/).map((val)=> {
+                return toImplicitType(val);
+            });
+            return parsed;
+        } else if (objRegex.test(data)) {
+            try {
                 converted = JSON.parse(data);
+            } catch (e) {
+                console.error('toImplicitType: couldn\'t convert', data);
             }
         }
-        return converted;
-    },
-
-    toOperationFormat: function (value) {
-        var split = (value || '').split('|');
-        var listOfOperations = split.map(function (value) {
-            value = value.trim();
-            var fnName = value.split('(')[0];
-            var params = value.substring(value.indexOf('(') + 1, value.indexOf(')'));
-            var args = params.trim() !== '' ? params.split(',') : [];
-
-            return { name: fnName, value: args };
-        });
-        return listOfOperations;
     }
-};
+    return converted;
+}
+
+function toOperationFormat(value) {
+    var split = (value || '').split('|');
+    var listOfOperations = split.map(function (value) {
+        value = value.trim();
+        var fnName = value.split('(')[0];
+        var params = value.substring(value.indexOf('(') + 1, value.indexOf(')'));
+        var args = params.trim() !== '' ? params.split(',') : [];
+
+        return { name: fnName, value: args };
+    });
+    return listOfOperations;
+}
+
+export { toImplicitType, toOperationFormat };
--- tests/specs/utils/test-parse-utils.js
+++ tests/specs/utils/test-parse-utils.js
@@ -1,47 +1,51 @@
 'use strict';
 
-var u = require('src/utils/parse-utils');
+import { toImplicitType, toOperationFormat } from 'src/utils/parse-utils';
 
 describe('parse utils', function () {
-    describe('#toImplicitType', function () {
+    describe.only('#toImplicitType', function () {
         it('identifies strings', function () {
-            u.toImplicitType('abc').should.equal('abc');
-            u.toImplicitType('').should.equal('');
+            toImplicitType('abc').should.equal('abc');
+            toImplicitType('').should.equal('');
         });
         it('identifies numbers', function () {
-            u.toImplicitType(1).should.equal(1);
-            u.toImplicitType(0).should.equal(0);
-            u.toImplicitType(-33423).should.equal(-33423);
+            toImplicitType(1).should.equal(1);
+            toImplicitType(0).should.equal(0);
+            toImplicitType(-33423).should.equal(-33423);
         });
 
         it('identifies numbers passed in as strings', function () {
-            u.toImplicitType('2').should.equal(2);
-            u.toImplicitType('0').should.equal(0);
+            toImplicitType('2').should.equal(2);
+            toImplicitType('0').should.equal(0);
         });
         it('identifies numbers passed in as quoted strings', function () {
-            u.toImplicitType('\'2\'').should.equal('2');
-            u.toImplicitType('"0"').should.equal('0');
+            toImplicitType('\'2\'').should.equal('2');
+            toImplicitType('"0"').should.equal('0');
         });
 
         it('identifies arrays', function () {
-            u.toImplicitType('[]').should.eql([]);
-            u.toImplicitType('[1, 2, 4]').should.eql([1, 2, 4]);
-            u.toImplicitType('[1, "2",4]').should.eql([1, '2', 4]);
+            toImplicitType('[]').should.eql([]);
+            toImplicitType('[1, 2, 4]').should.eql([1, 2, 4]);
+            toImplicitType('[1, "2",4]').should.eql([1, '2', 4]);
+            toImplicitType("[1, '2',4]").should.eql([1, '2', 4]);
 
-            u.toImplicitType(JSON.stringify([1, '2', 4])).should.eql([1, '2', 4]);
+            toImplicitType(JSON.stringify([1, '2', 4])).should.eql([1, '2', 4]);
         });
 
         it('identifies nulls', function () {
-            should.not.exist(u.toImplicitType(null));
-            should.not.exist(u.toImplicitType('null'));
-            u.toImplicitType('undefined').should.equal('');
-            should.not.exist(u.toImplicitType());
+            should.not.exist(toImplicitType(null));
+            should.not.exist(toImplicitType('null'));
+            toImplicitType('undefined').should.equal('');
+            should.not.exist(toImplicitType());
         });
 
         it('identifies objs', function () {
-            u.toImplicitType('{}').should.eql({});
-            u.toImplicitType('{"a": "abc"}').should.eql({ a: 'abc' });
-            u.toImplicitType('{"a": "abc", "b": 2,  "c": "2"}').should.eql({ a: 'abc', b: 2, c: '2' });
+            toImplicitType('{}').should.eql({});
+            toImplicitType('{"a": "abc"}').should.eql({ a: 'abc' });
+            toImplicitType('{"a": "abc", "b": 2,  "c": "2"}').should.eql({ a: 'abc', b: 2, c: '2' });
+        });
+        it('should not break on invalid objs', ()=> {
+            toImplicitType('{"a": "ab').should.eql('{"a": "ab');
         });
     });
 });
