1e3d09f603f4c6e6c25d5237fade18a62804fd0f
Naren <narendran.ranjit@gmail.com>
2017-08-17 15:37:02 -0700

    added stub addable

---
+++ src/utils/addable-hoc/index.js
@@ -0,0 +1,30 @@
+export function addable(obj, parser) {
+    const handlers = [];
+    const boundObj = Object.keys(obj).reduce((accum, key)=> {
+        const fn = obj[key];
+        accum[key] = fn.bind(fn, handlers);
+        return accum;
+    }, {});
+
+    return Object.assign({}, boundObj, {
+        list: ()=> handlers,
+        add: function (name, ...args) {
+            const parsed = parser(...args);
+            parsed.name = name;
+            handlers.push(parsed);
+        },
+        remove: function () {
+            handlers = handlers.filter((h)=> h.name !== name);
+        },
+        replace: function (name, newHandler) {
+            handlers = handlers.map((h)=> {
+                if (h.name === name) {
+                    const newHandler = parser(h);
+                    newHandler.name = name;
+                    return newHandler;
+                }
+                return h;
+            });
+        }
+    });
+}
