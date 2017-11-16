7f532f5d9d2605e3bd1915f37cb7adc41000e611
Naren <narendran.ranjit@gmail.com>
2017-11-16 12:24:22 -0800

    add tests for new collection converters

--- src/converters/collection-converters.js
+++ src/converters/collection-converters.js
@@ -1,34 +1,84 @@
 const _ = require('lodash');
 
 const list = {
-    except: function (valueToReject, val) {
-        return _.reject([].concat(val), valueToReject);
-    },
-    filter: function (value, val) {
-        return _.filter([].concat(val), value);
-    },
     /**
-     * Checks if array contains *any* truthy value
+     * Returns all values in array matching argument
      *
      * ** Example **
-     * <button data-f-disabled="users | any(isOnline) | not">Get Started</button>
-     * <button data-f-disabled="users | any({ isOnline: false })">Get Started</button>
+     * <!-- list online users -->
+     * <ul data-f-foreach="users | filter(isOnline)"><li></li></button>
+     * 
+     * @param  {string|number|boolean} valueToFilterBy value to reject
+     * @param  {any[]} source The arrayed model variable
+     * @return {any[]}     filtered lsit
+     */
+    filter: function (valueToFilterBy, source) {
+        source = [].concat(source);
+        if ($.isPlainObject(source[0])) {
+            return _.filter(source, valueToFilterBy);
+        }
+        return source.filter((v)=> v === valueToFilterBy);
+    },
+
+    /**
+     * Returns all values in array excluding match
+     *
+     * ** Example **
+     * <!-- list offline users -->
+     * <ul data-f-foreach="users | except(isOnline)"><li></li></button>
+     * 
+     * @param  {string|number|boolean} valueToReject value to reject
+     * @param  {any[]} source The arrayed model variable
+     * @return {any[]}     filtered lsit
+     */
+    except: function (valueToReject, source) {
+        source = [].concat(source);
+        if ($.isPlainObject(source[0])) {
+            return _.reject(source, valueToReject);
+        }
+        return source.filter((v)=> v !== valueToReject);
+    },
+
+    /**
+     * Checks if array contains *any* item passing the test
+     *
+     * ** Example **
+     * <!-- hides button if any of the users are not online -->
+     * <button data-f-hideif="users | any(isOnline) | not">Get Started</button>
+     * <button data-f-hideif="users | any({ isOnline: false })">Get Started</button>
      * 
      * @param  {string|object} value value to check for
-     * @param  {object[]} source The arrayed model variable
+     * @param  {any[]} source The arrayed model variable
      * @return {boolean}     True if match found in array
      */
     any: function (value, source) {
-        return _.some([].concat(source), value);
+        source = [].concat(source);
+        if ($.isPlainObject(source[0])) {
+            return _.some(source, value);
+        }
+        return source.indexOf(value) !== -1;
     },
+
+    /**
+     * Checks if *every* item in the array passes the test
+     *
+     * ** Example **
+     * <!-- shows button if any of the users are not online -->
+     * <button data-f-showif="users | every(isOnline)">Get Started</button>
+     * 
+     * @param  {string|object} value value to check for
+     * @param  {any[]} source The arrayed model variable
+     * @return {boolean}     True if match found in array
+     */
     every: function (value, source) {
-        return _.every([].concat(source), value);
-    },
-    find: function (value, source) {
-        return _.find([].concat(source), value);
-    },
+        source = [].concat(source);
+        if ($.isPlainObject(source[0])) {
+            return _.every(source, value);
+        }
+        return source.filter((v)=> v === value).length === source.length;
+    }
 };
-const converters = Object.values(list).map((name)=> {
+const converters = Object.keys(list).map((name)=> {
     return {
         alias: name,
         acceptList: true,
---
+++ src/converters/tests/test-collection-converters.js
@@ -0,0 +1,75 @@
+const cc = require('converters/collection-converters');
+
+function getConverter(name) {
+    const conv = cc.find((c)=> c.alias === name);
+    return conv.convert;
+}
+
+describe.only('Collection Converters', ()=> {
+    describe('#any', ()=> {
+        const any = getConverter('any');
+        const src = [{ name: 'John', isOnline: false }, { name: 'Wayne', isOnline: true }];
+        it('should allow string params', ()=> {
+            expect(any('isOnline', src)).to.equal(true);
+        });
+        it('should allow object matches', ()=> {
+            expect(any({ isOnline: true }, src)).to.equal(true);
+            expect(any({ foobar: true }, src)).to.equal(false);
+        });
+        it('should allow array of literals', ()=> {
+            const src = [1, 2, 3];
+            expect(any(1, src)).to.eql(true);
+            expect(any(10, src)).to.eql(false);
+        });
+    });
+    describe('#every', ()=> {
+        const every = getConverter('every');
+        const src = [{ name: 'John', isOnline: false, isStudent: true }, { name: 'Wayne', isOnline: true, isStudent: true }];
+        it('should allow string params', ()=> {
+            expect(every('isOnline', src)).to.equal(false);
+            expect(every('isStudent', src)).to.equal(true);
+        });
+        it('should allow object matches', ()=> {
+            expect(every({ isOnline: true }, src)).to.equal(false);
+            expect(every({ isStudent: true }, src)).to.equal(true);
+            expect(every({ foobar: true }, src)).to.equal(false);
+        });
+        it('should allow array of literals', ()=> {
+            expect(every(true, [true, true, true])).to.eql(true);
+            expect(every(true, [true, true, true, 1])).to.eql(false);
+            expect(every(false, [true, true, true, 1])).to.eql(false);
+        });
+    });
+    describe('#except', ()=> {
+        const except = getConverter('except');
+        describe('array of objects', ()=> {
+            const src = [{ name: 'John', isOnline: false, isStudent: true }, { name: 'Wayne', isOnline: true, isStudent: true }];
+            it('should allow string params', ()=> {
+                expect(except('isOnline', src)).to.eql([src[0]]);
+            });
+            it('should allow object matches', ()=> {
+                expect(except({ 'isOnline': false }, src)).to.eql([src[1]]);
+            });
+        });
+        it('should allow array of literals', ()=> {
+            const src = [1, 2, 3];
+            expect(except(1, src)).to.eql([2, 3]);
+        });
+    });
+    describe('#filter', ()=> {
+        const filter = getConverter('filter');
+        describe('array of objects', ()=> {
+            const src = [{ name: 'John', isOnline: false, isStudent: true }, { name: 'Wayne', isOnline: true, isStudent: true }];
+            it('should allow string params', ()=> {
+                expect(filter('isOnline', src)).to.eql([src[1]]);
+            });
+            it('should allow object matches', ()=> {
+                expect(filter({ 'isOnline': false }, src)).to.eql([src[0]]);
+            });
+        });
+        it('should allow array of literals', ()=> {
+            const src = [1, 2, 3];
+            expect(filter(1, src)).to.eql([1]);
+        });
+    });
+});
