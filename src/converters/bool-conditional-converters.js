8ebddabd2d30f1361c6015de7ed7396dd9116df0
Naren <narendran.ranjit@gmail.com>
2017-11-16 09:53:28 -0800

    test boolean converters

--- src/converters/bool-conditional-converters.js
+++ src/converters/bool-conditional-converters.js
@@ -47,9 +47,20 @@ module.exports = {
      * @param {Any} value
      * @return {Boolean}
      */
-    toBool: function (value) {
-        return !!value;
-    },
+    toBool: (value)=> !!value,
+
+    /**
+     * Converts values to boolean and negates.
+     *
+     * **Example**
+     *
+     *      <!-- disables input if isGameInProgress is false -->
+     *      <input type="text" data-f-disabled="isGameInProgress | not" />
+     * 
+     * @param {Any} value
+     * @return {Boolean}
+     */
+    not: (value)=> !value,
 
     /**
      * Convert the input to a new value (for example, some text), based on whether it is true or false.
