25eccebe8a1868021aa87a9910b7a57915906da2
Naren Ranjit <narendran.ranjit@gmail.com>
2018-03-08 21:26:59 -0800

    added json router test

---
+++ src/channels/middleware/epicenter-middleware/tests/test-json-router.js
@@ -0,0 +1,21 @@
+import { match } from '../json-router/index';
+
+describe('JSON Router', ()=> {
+    describe('#match', ()=> {
+        it('should not match topics which aren\'t json', ()=> {
+            const validMatches = ['[1,"a", b]', '{ "a": 1, "b": 2 }', 1];
+            const invalidMatches = ['a'];
+
+            validMatches.forEach((v)=> {
+                const ret = match(v);
+                expect(ret).to.eql(true);
+            });
+
+            invalidMatches.forEach((v)=> {
+                const ret = match(v);
+                expect(ret).to.eql(false);
+            });
+
+        });
+    });
+});
