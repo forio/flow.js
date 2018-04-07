9f7b453fedbef50c45a7fcc9e106a075b13b9787
Naren Ranjit <narendran.ranjit@gmail.com>
2018-04-06 17:02:38 -0700

    relocate parse utils

--- tests/test-list.js
+++ tests/test-list.js
@@ -8,7 +8,6 @@ var srcContext = require.context('../src/', true, /tests\/test-(.*)\.js$/i);
 srcContext.keys().forEach(srcContext);
 
 require('./specs/utils/test-general-utils');
-require('./specs/utils/test-parse-utils');
 require('./specs/dom/test-dom-manager'); //2.7
 require('./specs/dom/attributes/test-attribute-manager');
 require('./specs/dom/test-default-input-node');
