be79b23de8360bfe7cdbbbd190e3bb8396335997
Naren Ranjit <narendran.ranjit@gmail.com>
2018-07-30 22:24:10 -0700

    remove unused player handler

--- src/channels/route-handlers/epicenter-route-handlers/world-route-handler/consensus-route-handler/index.js
+++ src/channels/route-handlers/epicenter-route-handlers/world-route-handler/consensus-route-handler/index.js
@@ -1,6 +1,5 @@
 import OperationsHandler from './consensus-operations-handler';
 import StatusHandler from './consensus-status-handler';
-import PlayersHandler from './consensus-players-handler';
 import router from 'channels/channel-router';
 import { matchPrefix } from 'channels/route-handlers/route-matchers';
 import { withPrefix } from 'channels/channel-router/utils';
@@ -8,7 +7,6 @@ import { withPrefix } from 'channels/channel-router/utils';
 const ConsensusManager = F.manager.ConsensusManager;
 
 const OPERATIONS_PREFIX = 'operations:';
-const PLAYERS_PREFIX = 'players:';
 const STATUS_PREFIX = 'status:';
 
 export default function ConsensusRouteHandler(config, notifier) {
@@ -39,7 +37,6 @@ export default function ConsensusRouteHandler(config, notifier) {
 
     const opnHandler = OperationsHandler(handlerOptions, withPrefix(notifier, OPERATIONS_PREFIX));
     const statusHandler = StatusHandler(handlerOptions, withPrefix(notifier, STATUS_PREFIX));
-    const playersHandler = PlayersHandler(handlerOptions, withPrefix(notifier, PLAYERS_PREFIX));
 
     const handlers = [
         $.extend({}, opnHandler, { 
@@ -52,12 +49,6 @@ export default function ConsensusRouteHandler(config, notifier) {
             match: matchPrefix(STATUS_PREFIX),
             options: handlerOptions.channelOptions.status,
         }),
-        $.extend({}, playersHandler, { 
-            isDefault: true,
-            name: 'consensus players',
-            match: matchPrefix(PLAYERS_PREFIX),
-            options: handlerOptions.channelOptions.players,
-        }),
     ];
 
     getWorld().then((world)=> {
@@ -65,7 +56,6 @@ export default function ConsensusRouteHandler(config, notifier) {
         channel.subscribe(channel.TOPICS.RUN_RESET, ()=> {
             getConsensus(true).then((consensus)=> {
                 statusHandler.notify(consensus);
-                playersHandler.notify(consensus);
             });
         });
         // FIXME: Filter by stage here
@@ -74,11 +64,9 @@ export default function ConsensusRouteHandler(config, notifier) {
             if (isComplete) {
                 getConsensus(true).then((consensus)=> {
                     statusHandler.notify(consensus);
-                    playersHandler.notify(consensus);
                 });
             } else {
                 statusHandler.notify(consensus);
-                playersHandler.notify(consensus);
             }
         });
     });
